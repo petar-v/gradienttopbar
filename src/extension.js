022d9cda58f93d23eff080093ab6fae8af536eb3
Petar Vasilev <petar@pshow.org>
2023-05-02 19:33:08 +0100

    remove gradient on full screen

--- src/extension.js
+++ src/extension.js
@@ -1,25 +1,50 @@
-// Based on the original extension at https://extensions.gnome.org/extension/1264/gradient-top-bar/
-
 const Main = imports.ui.main;
+const { Meta } = imports.gi;
 
 const GRADIENT_CLASS = "panel-gradient";
 const CORNER_GRADIENT_CLASS = "corner-gradient";
 
-const toggleCorners = (enable) =>
+let isFaded = false;
+const toggleGradient = (enabled) => {
+  if (enabled === isFaded) {
+    return;
+  }
+  const actionCall = enabled
+    ? "add_style_class_name"
+    : "remove_style_class_name";
+  Main.panel.actor[actionCall](GRADIENT_CLASS);
   [Main.panel._leftCorner, Main.panel._rightCorner].forEach(
-    (corner) =>
-      corner &&
-      corner[enable ? "add_style_class_name" : "remove_style_class_name"](
-        CORNER_GRADIENT_CLASS
-      )
+    (corner) => corner && corner[actionCall](CORNER_GRADIENT_CLASS)
   );
 
+  isFaded = enabled;
+};
+
+const { HORIZONTAL, VERTICAL, BOTH } = Meta.MaximizeFlags;
+const isMaximized = (window) => window.get_maximized() === BOTH;
+
+const maximizedWindows = new Set();
+
+const onWindowSizeChange = (window) => {
+  if (isMaximized(window)) {
+    maximizedWindows.add(window.get_id());
+  } else {
+    maximizedWindows.delete(window.get_id());
+  }
+  toggleGradient(maximizedWindows.size === 0);
+};
+
+let windowCreatedId;
 function enable() {
-  Main.panel.actor.add_style_class_name(GRADIENT_CLASS);
-  toggleCorners(true);
+  windowCreatedId = global.display.connect("window-created", (_, win) => {
+    if (win.can_maximize()) {
+      win.connect("size-changed", onWindowSizeChange);
+    }
+  });
+  toggleGradient(true);
 }
 
 function disable() {
-  Main.panel.actor.remove_style_class_name(GRADIENT_CLASS);
-  toggleCorners(false);
+  global.display.disconnect(windowCreatedId);
+  toggleGradient(false);
 }
