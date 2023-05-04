28169b92c32140b8e3e59159b0e03ea33f094db8
Petar Vasilev <petar@pshow.org>
2023-05-04 17:31:03 +0100

    general page

---
+++ src/settings/behavioursPage.js
@@ -0,0 +1,48 @@
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+const { Adw, Gdk, Gio, GLib, GObject, Gtk } = imports.gi;
+const Gettext = imports.gettext.domain(Me.metadata["gettext-domain"]);
+const { gettext } = Gettext;
+
+var BehaviourPage = GObject.registerClass(
+  class Behaviour extends Adw.PreferencesPage {
+    _init(settings) {
+      super._init({
+        title: gettext("Behaviour"),
+        icon_name: 'system-run-symbolic',
+        name: "Behaviour",
+      });
+
+      this._settings = settings;
+      this.widgetRows = [];
+
+      const behaviorGroup = new Adw.PreferencesGroup({
+        title: gettext("Behavior"),
+      });
+
+      let opaqueOnMaximizedSwitch = new Gtk.Switch({
+        valign: Gtk.Align.CENTER,
+        active: this._settings.get_boolean("opaque-on-maximized"),
+      });
+      settings.bind(
+        "opaque-on-maximized",
+        opaqueOnMaximizedSwitch,
+        "active",
+        Gio.SettingsBindFlags.DEFAULT
+      );
+      let opaqueOnMaximizedRow = new Adw.ActionRow({
+        title: gettext("Opaque on maximized window"),
+        subtitle: gettext(
+          "Removes the gradient effect whenever there is a maximized window on the current workspace."
+        ),
+        activatable_widget: opaqueOnMaximizedSwitch,
+      });
+      opaqueOnMaximizedRow.add_suffix(opaqueOnMaximizedSwitch);
+      opaqueOnMaximizedRow.activatable_widget = opaqueOnMaximizedSwitch;
+
+      behaviorGroup.add(opaqueOnMaximizedRow);
+      this.add(behaviorGroup);
+    }
+  }
+);
