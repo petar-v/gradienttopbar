ad729e9048774d71b2b85efa96af05b20ddc6a2d
Petar Vasilev <petar@pshow.org>
2023-05-04 17:31:25 +0100

    add pages

--- src/prefs.js
+++ src/prefs.js
@@ -6,38 +6,23 @@ const Gettext = imports.gettext.domain(Me.metadata["gettext-domain"]);
 const { gettext } = Gettext;
 
 const { AboutPage } = Me.imports.settings.aboutPage;
+const { BehaviourPage } = Me.imports.settings.behavioursPage;
 
 function init() {
   ExtensionUtils.initTranslations();
-}
-
-function fillPreferencesWindow(window) {
   const iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default());
   if (!iconTheme.get_search_path().includes(`${Me.path}/assets`))
     iconTheme.add_search_path(`${Me.path}/assets`);
+}
 
-  const settings = {}; // ExtensionUtils.getSettings();
+function fillPreferencesWindow(window) {
+  const settings = ExtensionUtils.getSettings();
 
   window.can_navigate_back = true;
 
-  const homePage = new HomePage(settings);
+  const homePage = new BehaviourPage(settings);
   window.add(homePage);
 
   const aboutPage = new AboutPage();
   window.add(aboutPage);
 }
-
-var HomePage = GObject.registerClass(
-  class Behaviour extends Adw.PreferencesPage {
-    _init(settings) {
-      super._init({
-        title: gettext("Behaviour"),
-        icon_name: "preferences-system-symbolic",
-        name: "Behaviour",
-      });
-
-      this._settings = settings;
-      this.widgetRows = [];
-    }
-  }
-);
