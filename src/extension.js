30bcb2d58ef26de162e8be15bdaf6cd15e05a659
Petar Vasilev <petar@pshow.org>
2023-05-05 05:28:51 +0100

    wip and debug info

--- src/extension.js
+++ src/extension.js
@@ -29,10 +29,17 @@ const modifyTopBar = () => {
     .list_windows()
     .map((win) => win.get_id());
 
+  log("mofigying top bar --------------------------------------------------");
+
+  log("workspaceWindowIds", workspaceWindowIds);
+  log("maximizedWindows", maximizedWindows.size);
+
   const lacksWorkspaceMaximizedWindow =
     workspaceWindowIds.find((workspaceWindowId) =>
       maximizedWindows.has(workspaceWindowId)
     ) === undefined;
+
+  log("enabled", lacksWorkspaceMaximizedWindow);
 
   toggleGradient(lacksWorkspaceMaximizedWindow);
 };
@@ -40,6 +47,7 @@ const modifyTopBar = () => {
 const onWindowSizeChange = (window) => {
   if (isMaximized(window)) {
     maximizedWindows.add(window.get_id());
+    log("maximized window",  window.get_title());
   } else {
     maximizedWindows.delete(window.get_id());
   }
@@ -60,6 +68,19 @@ let workspaceSwitchId;
 function enable() {
   // listen for window created events and attach a size change event listener
   windowCreatedId = global.display.connect("window-created", (_, win) => {
+    /**
+    * This is a hack to prevent this extension from interfering with Desktop Icons NG.
+    * For more information, please see :
+    * https://www.reddit.com/r/pop_os/comments/utrdzl/00bdhf_on_the_alttab_app_list/
+    * https://askubuntu.com/questions/1345101/what-is-72-27bdh-and-0-27bdh
+    * https://unix.stackexchange.com/questions/680497/can-someone-explain-what-is-this-non-clickable-permanent-window-called-0-35bd
+    */
+    const isDesktopIconsNG = win.get_title() === "@!0,0;BDHF";
+    if(isDesktopIconsNG) {
+      return;
+    }
+
+    // attach an event listener only to maximizable windows
     if (win.can_maximize()) {
       // this is probably not the proper event to listen to but there was no "maximize" event
       // so this gets triggered every time there is a window resize. This is NOT optimal :(
