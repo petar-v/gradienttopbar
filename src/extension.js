fb407fb5c8d7de77a4c7b9b1cf6dbd9632286b0a
Petar Vasilev <petar@pshow.org>
2023-10-03 08:05:57 +0000

    Gnome minimized from maximized (#27)
    * versions
    
    * upgrade deps
    
    * upgrade eslint
    
    * new eslint config
    
    * convert config
    
    * convert prefs and deps
    
    * add wayland util
    
    * fix the extension.js
    
    * remove weird currying and fix a maximization issue
    
    * fix the settings references problem
    
    * addressed review comments
    
    * version bump
    
    * separate the window states from the main extension
    
    * removed a deprecation
    
    * fixed some issues after testing
    
    * handle minimization
    
    * add some todo-s
    
    * another edge case
    
    * fix a settings regression
    
    * more comments
    
    * version bump

--- src/extension.js
+++ src/extension.js
@@ -1,4 +1,3 @@
-import Meta from 'gi://Meta';
 import {
     Extension
 } from 'resource:///org/gnome/shell/extensions/extension.js';
@@ -9,48 +8,15 @@ import {
     detachSettingsListeners
 } from './config.js';
 
-const { BOTH } = Meta.MaximizeFlags;
-const isMaximized = window => window.get_maximized() === BOTH; // TODO: what if the window starts as maximized dimensions but is not "snapped"?
+import WindowEvents from './windowEvents.js';
 
-// TODO: separate the window states from the main extension
 export default class GradientTopBar extends Extension {
     constructor(metadata) {
         super(metadata);
 
         // gradient state
         this.isEffectApplied = false;
-
-        // window event listeners IDs
-        this.windowCreatedId = null;
-        this.workspaceSwitchId = null;
-        this.windowDestroyedId = null;
-
-        // window states
-        this.workspace = global.get_workspace_manager().get_active_workspace();
-        this.maximizedWindows = new Set();
-        this.monitoredWindows = {};
-
-        // event listener callbacks
-        this.onWindowSizeChange = window => {
-            if (isMaximized(window))
-                this.maximizedWindows.add(window.get_id());
-            else
-                this.maximizedWindows.delete(window.get_id());
-
-            this.modifyTopBar();
-        };
-
-        this.onWorkspaceChanged = workspaceManager => {
-            this.workspace = workspaceManager.get_active_workspace();
-            this.modifyTopBar();
-        };
-
-        this.onWindowDestroy = (_, windowActor) => {
-            const windowId = windowActor.get_meta_window().get_id();
-            this.maximizedWindows.delete(windowId);
-            delete this.monitoredWindows[windowId];
-            this.modifyTopBar();
-        };
+        this.windowEvents = null;
 
         this.onSettingsChanged = settings => {
             const config = getConfig(settings);
@@ -59,87 +25,12 @@ export default class GradientTopBar extends Extension {
             const { isOpaqueOnMaximized } = config;
 
             if (isOpaqueOnMaximized) {
-                this.enableMaximizedListeners();
+                this.windowEvents.enable();
             } else {
-                this.disableMaximizedListeners();
+                this.windowEvents.disable();
                 this.toggleGradient(true);
             }
         };
-
-        this.monitorSizeChange = win => {
-            // this is probably not the proper event to listen to but there was no "maximize" event
-            // so this gets triggered every time there is a window resize. This is NOT optimal :(
-            this.monitoredWindows[win.get_id()] = win.connect(
-                'size-changed',
-                this.onWindowSizeChange
-            );
-        };
-
-        this.onWindowCreate = (_, window) => {
-            if (window.can_maximize())
-                this.monitorSizeChange(window);
-
-            if (isMaximized(window))
-                this.maximizedWindows.add(window.get_id());
-
-            this.modifyTopBar();
-        };
-    }
-
-    modifyTopBar() {
-        const workspaceWindowIds = this.workspace
-        .list_windows()
-        .map(win => win.get_id());
-
-        const lacksWorkspaceMaximizedWindow =
-        workspaceWindowIds.find(workspaceWindowId =>
-            this.maximizedWindows.has(workspaceWindowId)
-        ) === undefined;
-
-        this.toggleGradient(lacksWorkspaceMaximizedWindow);
-    }
-
-    enableMaximizedListeners() {
-        if (!this.windowCreatedId) {
-            // listen for window created events and attach a size change event listener
-            this.windowCreatedId = global.display.connect('window-created', this.onWindowCreate);
-        }
-        if (!this.windowDestroyedId) {
-            this.windowDestroyedId = global.window_manager.connect(
-                'destroy',
-                this.onWindowDestroy
-            );
-        }
-        global.display
-        .list_all_windows()
-        .filter(window => this.monitoredWindows[window.get_id()] === undefined)
-        .forEach(this.monitorSizeChange);
-
-        if (!this.workspaceSwitchId) {
-        // keep a reference to the current workspace
-            this.workspaceSwitchId = global
-          .get_workspace_manager()
-          .connect('workspace-switched', this.onWorkspaceChanged);
-        }
-    }
-
-    disableMaximizedListeners() {
-        if (this.windowCreatedId) {
-            global.display.disconnect(this.windowCreatedId);
-            this.windowCreatedId = null;
-        }
-        if (this.workspaceSwitchId) {
-            global.get_workspace_manager().disconnect(this.workspaceSwitchId);
-            this.workspaceSwitchId = null;
-        }
-        if (this.windowDestroyedId) {
-            global.window_manager.disconnect(this.windowDestroyedId);
-            this.windowDestroyedId = null;
-        }
-        global.display.list_all_windows().forEach(window => {
-            window.disconnect(this.monitoredWindows[window.get_id()]);
-        });
-        this.monitoredWindows = {};
     }
 
     toggleGradient(enabled) {
@@ -156,13 +47,26 @@ export default class GradientTopBar extends Extension {
 
     enable() {
         this._settings = this.getSettings();
+        // TODO: always apply the gradient in overview, regardless of minimized or maximized windows
+        // TODO: make sure the maximized window is in the same monitor in case of multi-monitor setup
+
+        this.windowEvents = new WindowEvents(global.display, global.window_manager, global.get_workspace_manager());
+        this.windowEvents.setStateChangeCallback(({ maximizedWindows, currentWorkspace }) => {
+            const workspaceWindowIds = currentWorkspace.list_windows().map(win => win.get_id());
+
+            const lacksWorkspaceMaximizedWindow = workspaceWindowIds.find(workspaceWindowId =>
+                maximizedWindows.has(workspaceWindowId)
+            ) === undefined;
+
+            this.toggleGradient(lacksWorkspaceMaximizedWindow);
+        });
 
         attachSettingsListeners(this._settings, this.onSettingsChanged);
 
         const config = getConfig(this._settings);
         const { isOpaqueOnMaximized } = config;
         if (isOpaqueOnMaximized)
-            this.enableMaximizedListeners();
+            this.windowEvents.enable();
 
         // initially set up the gradient
         applyGradientStyle(config, this.path);
@@ -170,9 +74,11 @@ export default class GradientTopBar extends Extension {
     }
 
     disable() {
-        this.disableMaximizedListeners();
+        this.windowEvents.disable();
         this.toggleGradient(false);
         detachSettingsListeners(this._settings, this.onSettingsChanged);
+        this.windowEvents = null;
+        this.isEffectApplied = false;
         this._settings = null;
     }
 }
---
+++ src/windowEvents.js
@@ -0,0 +1,191 @@
+import Meta from 'gi://Meta';
+
+// I've created this file with the wish that I delete it one day (or at least make it much better).
+
+const { BOTH } = Meta.MaximizeFlags;
+
+const isMaximized = window => window.get_maximized() === BOTH;
+
+const SIZE_CHANGE_EVENT = 'size-changed';
+const WORKSPACE_CHANGE_EVENT = 'workspace-switched';
+const WINDOW_CREATE_EVENT = 'window-created';
+const WINDOW_DESTROY_EVENT = 'destroy';
+const WINDOW_MINIMIZED_EVENT = 'minimize';
+const WINDOW_RAISED_EVENT = 'unminimize';
+
+class EventManager {
+    constructor() {
+        this.evenIds = {}; // event name -> {target, eventID}
+        this.monitoredWindows = {}; // windowID -> {eventName -> eventID}
+    }
+
+    attachGlobalEventOnce(eventName, target, callback) {
+        if (this.evenIds[eventName] === undefined) {
+            const id = target.connect(eventName, callback);
+            this.evenIds[eventName] = {
+                target,
+                id
+            };
+        }
+    }
+
+    disconnectAllEvents() {
+        Object.keys(this.evenIds).forEach(eventName => {
+            const event = this.evenIds[eventName];
+            event.target.disconnect(event.id);
+        });
+        this.evenIds = {};
+    }
+
+    attachWindowEventOnce(eventName, window, callback) {
+        const windowId = window.get_id();
+        if (this.monitoredWindows[windowId] === undefined)
+            this.monitoredWindows[windowId] = {};
+
+        if (this.monitoredWindows[windowId][eventName] === undefined) {
+            this.monitoredWindows[windowId][eventName] = window.connect(
+                eventName,
+                callback
+            );
+        }
+    }
+
+    disconnectWindowEvents(window) {
+        const windowId = window.get_id();
+        if (this.monitoredWindows[windowId] === undefined)
+            return;
+
+        Object.keys(this.monitoredWindows[windowId]).forEach(eventName => window.disconnect(this.monitoredWindows[windowId][eventName]));
+        delete this.monitoredWindows[windowId];
+    }
+}
+
+const eqSet = (as, bs) => {
+    if (as.size !== bs.size)
+        return false;
+
+    for (const a of as) {
+        if (!bs.has(a))
+            return false;
+    }
+    return true;
+};
+
+const areSameState = (state1, state2) => {
+    if ([state1, state2].includes(null))
+        return false;
+    if ([state1.workspace, state2.workspace].includes(undefined))
+        return false;
+    if (state1.workspace.index() !== state2.workspace.index())
+        return false;
+
+    return eqSet(state1.maximizedWindows, state2.maximizedWindows);
+};
+
+export default class WindowEvents {
+    constructor(display, windowManager, workspaceManager) {
+        this.display = display;
+        this.windowManager = windowManager;
+        this.workspaceManager = workspaceManager;
+
+        this.eventManager = new EventManager();
+
+        this.stateChangeCallback = () => {};
+
+        this.workspace = null;
+        this.maximizedWindows = new Set();
+    }
+
+    setStateChangeCallback(callback) {
+        this.stateChangeCallback = callback;
+    }
+
+    enable() {
+        let lastState = null;
+        const emitStateChange = () => {
+            const currentState = {
+                maximizedWindows: this.maximizedWindows,
+                currentWorkspace: this.workspace
+            };
+            if (!areSameState(lastState, currentState)) {
+                lastState = currentState;
+                this.stateChangeCallback(currentState);
+            }
+        };
+
+        const onWindowSizeChange = window => {
+            if (isMaximized(window))
+                this.maximizedWindows.add(window.get_id());
+            else
+                this.maximizedWindows.delete(window.get_id());
+
+            emitStateChange();
+        };
+
+        const onWorkspaceChanged = workspaceManager => {
+            this.workspace = workspaceManager.get_active_workspace();
+            emitStateChange();
+        };
+
+        const onWindowDestroy = (_, windowActor) => {
+            const window = windowActor.get_meta_window();
+            this.maximizedWindows.delete(window.get_id());
+            this.eventManager.disconnectWindowEvents(window);
+
+            emitStateChange();
+        };
+
+        const onWindowCreate = (_, window) => {
+            if (window.can_maximize())
+                this.eventManager.attachWindowEventOnce(SIZE_CHANGE_EVENT, window, onWindowSizeChange);
+
+            if (isMaximized(window))
+                this.maximizedWindows.add(window.get_id());
+
+            emitStateChange();
+        };
+
+        const onWindowMinimize = (_, windowActor) => {
+            const windowId = windowActor.get_meta_window().get_id();
+            this.maximizedWindows.delete(windowId);
+            emitStateChange();
+        };
+
+        const onWindowRaise = (_, windowActor) => {
+            const window = windowActor.get_meta_window();
+            if (isMaximized(window))
+                this.maximizedWindows.add(window.get_id());
+            emitStateChange();
+        };
+
+        // TODO: what if the window starts as maximized dimensions but is not "snapped"?
+        // TODO: what if we have tiled windows?
+
+        // FIXME: add events for when switching maximized windows from one workspace to another. I think in that case I only need to emit a state change
+        // https://gjs-docs.gnome.org/meta13~13/meta.workspace#signal-window-added
+        // https://github.com/AMDG2/GnomeShell_DynamicTopBar/blob/906b8e69479bd6098748752ee8842782177aa7dd/dynamicTopBar%40gnomeshell.feildel.fr/extension.js#L288
+        this.eventManager.attachGlobalEventOnce(WINDOW_CREATE_EVENT, this.display, onWindowCreate);
+        this.eventManager.attachGlobalEventOnce(WINDOW_DESTROY_EVENT, this.windowManager, onWindowDestroy);
+        this.eventManager.attachGlobalEventOnce(WORKSPACE_CHANGE_EVENT, this.workspaceManager, onWorkspaceChanged);
+        this.eventManager.attachGlobalEventOnce(WINDOW_MINIMIZED_EVENT, this.windowManager, onWindowMinimize);
+        this.eventManager.attachGlobalEventOnce(WINDOW_RAISED_EVENT, this.windowManager, onWindowRaise);
+
+        // TODO: instead of on size change, listen for https://gjs-docs.gnome.org/meta13~13/meta.window#property-maximized_horizontally or vertically
+        // to make it work with tiling, I would need to figure out the position in case it is maximized horizontally but on top.
+        // if it's maximized vertically, then it's likely on either side. In that case I want to make the bar opaque.
+        this.display.list_all_windows().forEach(window => this.eventManager.attachWindowEventOnce(SIZE_CHANGE_EVENT, window, onWindowSizeChange));
+
+        this.workspace = this.workspaceManager.get_active_workspace();
+        this.maximizedWindows = new Set(this.display.list_all_windows().filter(isMaximized).map(window => window.get_id()));
+        emitStateChange();
+    }
+
+    disable() {
+        this.eventManager.disconnectAllEvents();
+        this.display.list_all_windows().forEach(window => {
+            this.eventManager.disconnectWindowEvents(window);
+        });
+        this.workspace = null;
+        this.maximizedWindows = new Set();
+    }
+}
