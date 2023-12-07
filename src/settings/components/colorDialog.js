54474a991768a4169110f57a2db43dee662fe86c
Petar Vasilev <petar@pshow.org>
2023-12-07 17:12:47 +0000

    removed a legacy toolkit thing

--- src/settings/components/colorDialog.js
+++ src/settings/components/colorDialog.js
@@ -1,10 +1,7 @@
 import Gtk from 'gi://Gtk';
 import Gdk from 'gi://Gdk';
 
-export const createColorDialogBtn = (pickerTitle, colorString, callback) => {
-    if ([Gtk.ColorDialog, Gtk.ColorDialogButton].includes(undefined))
-        return null;
-
+export const createColorDialog = (pickerTitle, colorString, callback) => {
     const colorDialog = Gtk.ColorDialog.new();
     colorDialog.set_title(pickerTitle);
     colorDialog.set_modal(true);
@@ -20,27 +17,4 @@ export const createColorDialogBtn = (pickerTitle, colorString, callback) => {
     });
 
     return chooserBtn;
-};
-
-export const createColorDialogBtnLegacy = (
-        pickerTitle,
-        colorString,
-        callback
-) => {
-    const rgba = new Gdk.RGBA();
-    rgba.parse(colorString);
-    const button = Gtk.ColorButton.new_with_rgba(rgba);
-    button.set_modal(true);
-    button.set_title(pickerTitle);
-    button.connect('color-set', btn => {
-        callback(btn.get_rgba().to_string());
-    });
-    return button;
-};
-
-export const createColorDialog = (pickerTitle, colorString, callback) => {
-    const dialog = createColorDialogBtn(pickerTitle, colorString, callback);
-    if (dialog)
-        return dialog;
-    return createColorDialogBtnLegacy(pickerTitle, colorString, callback);
 };
